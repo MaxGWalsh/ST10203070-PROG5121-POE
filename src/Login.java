2227c2405861cc24b91096ae2a63ec8fce570e41
MaxGWalsh <maxgwalsh@gmail.com>
2022-03-29 16:44:52 +0200

    PROG POE
    POE

---
+++ src/Login.java
@@ -0,0 +1,49 @@
+
+// @author Max Walsh
+public class Login
+    {
+
+    //-----------------------------------------//
+    //main method
+    public static void main(String[] args)
+        {
+
+        }
+
+    //-----------------------------------------//
+    //check username method
+    public static boolean checkUserName()
+        {
+
+        }
+
+    //-----------------------------------------//
+    //check password method
+    public static boolean checkPasswordComplexity()
+
+        {
+
+        }
+
+    //------------------------------------------//
+    //register user method
+    public static String registerUser()
+        {
+
+        }
+
+    //------------------------------------------//
+    //login user method
+    public static boolean loginUser()
+        {
+
+        }
+
+    //------------------------------------------//
+    //return login status method
+    public static String returnLoginStatus()
+        {
+
+        }
+
+    }
