3d27d5dac9eb8c50c080079e558e33c8a6a648dc
MaxGWalsh <st10203070@vcconnect.edu.za>
2022-04-28 09:48:35 +0200

    initial commit

---
+++ src/st10203070/Message.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author maxgw
+ */
+public class Message
+{
+    String returnMessage = "I have arrived";
+
+    public String getMessage()
+    {
+        return returnMessage;
+    }
+}
